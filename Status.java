7aca48b32cdfdd9d880923350eb2c0238f4d8a31
prabhakar162825-oss <prabhakar162825@gmail.com>
2026-03-05 13:42:51 +0530

    Update Status.java

--- Status.java
+++ Status.java
@@ -1,22 +1,35 @@
 package com.index.pack;
-import java.util.Scanner;
-public class Status {
-public static void main(String args[]) {
-	Scanner sc=new Scanner(System.in);
-	String Username=sc.nextLine();
-	System.out.println("Enter ur Username:");
-	String Password=sc.nextLine();
-	System.out.println("Enter Ur Password");
-	
-	String Status;
 
-	if (Username.equals("Prabha") && Password.equals("Prabha@28")) {
-	    Status = "Login Successful";
+	import java.util.Scanner;
+	public class Status {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter ur Username:");
+		String Username=sc.nextLine();
+		System.out.println("Enter Ur Password");
+		String Password=sc.nextLine();
+		
+		
+		String Validation;
+
+		if (Username.equals("Prabha") && Password.equals("Pass28")) {
+		    Validation = "Login Successful";
+		}
+		else if 
+		 (!Username.equals("Prabha") && Password.equals("Pass28")) {
+		    Validation = "Incorrect Username";
+			}
+		else if 
+		(Username.equals("Prabha") && !Password.equals("Pass28")) {
+		    Validation = "Incorrect Password";
+		}
+		else {
+		
+			Validation = "Enter Valid Login credentials";
+		}
+		
+		System.out.println(Validation);
 
 
-	System.out.println(Status);
-
-
-}
-}
-}
+	}
+	}
